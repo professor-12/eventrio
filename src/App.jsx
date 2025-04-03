8bf30e9725c3cc81374c30081b20ac6542bad0e7
emmanuel <badejoemmanuel805@gmail.com>
2025-04-03 16:29:18 +0100

    Add login and register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
+import Auth from './layouts/Auth'
+import LoginPage from './pages/login.page'
+import Regsiter from './pages/register.page'
 
 function App() {
-  const route = createBrowserRouter([])
-
+  const route = createBrowserRouter([{
+    path: "/auth",
+    element: <Auth />,
+    children: [
+      {
+        path: "login",
+        element: <LoginPage />
+      },
+      {
+        path: "register",
+        element: <Regsiter />
+      },
+    ]
+  }])
   return <RouterProvider router={route}></RouterProvider>
 }
 
